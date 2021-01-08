6f4f88c23e91282e5558a5710d22d8b84c31bc03
carryex <pokinsokha@gmail.com>
2021-01-08 20:03:25 +0100

    add redux-thunk to middleware. Change add fetchCollectionStartAsync for get data and change collections isFetching status. Add new action to shop.actions and chenge shop.type. Change cases in shop.reducer

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,35 @@
 import ShopActionsTypes from "./shop.types";
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionsTypes.UPDATE_COLLECTIONS,
+import {
+  firestore,
+  converCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionsTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionsTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap,
 });
+
+export const fetchCollectionsFailuer = (errorMessage) => ({
+  type: ShopActionsTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart());
+
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionsMap = converCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailuer(error.message)));
+  };
+};
